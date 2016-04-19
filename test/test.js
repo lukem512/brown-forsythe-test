d158586c700bf7e531f5f36cd89225c4bed59e27
lukem512 <lukem95@gmail.com>
2016-04-19 11:14:54 +0100

    Added tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,18 @@
 var bf = require('../src/brown-forsythe');
 
 var tests = [
-	// TODO
+	{
+		name: 'Simple Brown-Forsythe Test #1',
+		expected: 1.5235,
+		rejected: false,
+		samples: [
+			[65, 61, 81, 88, 69, 89, 55, 84, 86, 84, 71],
+			[77, 74, 80, 76, 77, 93, 64, 83, 75, 82, 70],
+			[69, 70, 71, 80, 74, 78, 60, 80, 81, 86, 73],
+			[75, 66, 74, 88, 69, 77, 50, 77, 87, 92, 81],
+			[69, 68, 79, 79, 76, 80, 63, 78, 79, 85, 79]
+		]
+	}
 ];
 
 var returnCode = 0;
